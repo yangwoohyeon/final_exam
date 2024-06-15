1fe86daf32fa4b17c05353f5eb4be905bf489f75
yangwoohyeon <dngus0717@gmail.com>
2024-06-16 00:23:07 +0900

    exam

--- src/CarTest.java
+++ src/CarTest.java
@@ -1,6 +1,6 @@
 public class CarTest { //테스트 클래스
     public static void main(String[] args) {
-        Car obj = new Car();
+        Car obj = new Car(); //객체 생성
         System.out.println(obj.getClass());
     }
 }
